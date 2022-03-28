eec5f14ab9c54e2228759bcfdf859d65e459ea0e
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-28 15:20:11 -0300

    taskList

---
+++ scripts.js
@@ -0,0 +1,60 @@
+let input = document.getElementById('input-principal')
+let button = document.getElementById('botao-adicionar')
+let task = document.getElementById('nome-tarefa-id')
+let fullList = document.getElementById('tarefas')
+
+let arrayAddTask = []
+reloadTask()
+
+function viewTask() {
+  let newLi = ''
+  arrayAddTask.forEach((addTask, index) => {
+
+    newLi = newLi + `
+    <li class="item-tarefa ${addTask.finish == true && "concluido"}">
+    
+    <button class="botao-arrow" onclick="completeTask(${index})"><i class="fa-solid fa-circle-arrow-right"></i></button>
+    
+    <p class="nome-tarefa ${addTask.finish == true && "concluido"}" id="nome-tarefa-id">${addTask.addTask}</p>
+    
+    <button class="botao-delete" onclick="delTask(${index})"><i class="fa-solid fa-trash"></i></button>
+    </li>
+    `
+  })
+
+  fullList.innerHTML = newLi
+  localStorage.setItem('lista', JSON.stringify(arrayAddTask))
+
+}
+
+function delTask(index) {
+  arrayAddTask.splice(index, 1)
+  viewTask()
+}
+
+function addTask() {
+  arrayAddTask.push({
+    addTask: input.value,
+    finish: false
+  })
+
+  console.log(arrayAddTask)
+  viewTask()
+}
+
+function completeTask(index) {
+  arrayAddTask[index].finish = !arrayAddTask[index].finish
+
+  viewTask()
+}
+
+function reloadTask() {
+  let myTask = localStorage.getItem('lista')
+
+  arrayAddTask = JSON.parse(myTask)
+
+  viewTask()
+}
+
+button.addEventListener('click', addTask)
+
